eed737a7fa115cd015406a56b3438fcd101c03ce
uncox-ali <alif.mohamady20@gmail.com>
2026-03-21 18:26:57 +0330

    Final cleanup of temporary scripts

--- gen_all_hashes.js
+++
@@ -1,6 +0,0 @@
-const bcrypt = require('bcryptjs');
-['admin', 'editor', 'viewer'].forEach(password => {
-    bcrypt.hash(password, 10).then(hash => {
-        console.log(`${password}: ${hash}`);
-    });
-});
